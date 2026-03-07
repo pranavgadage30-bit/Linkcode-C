5a4514ed542e9eb2d82ffba56c7d4a26efd01d3e
Pranav Gadage <pranavgadage30@gmail.com>
2026-03-07 11:40:14 +0530

    Add files via upload

---
+++ 11pattern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n = 4, i, j;
+
+    // Loop for row
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+//**** **** **** ****
