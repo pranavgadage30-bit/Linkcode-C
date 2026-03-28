0bba82037a12ee26ad536c2105e666b022fc16fb
Pranav Gadage <pranavgadage30@gmail.com>
2026-03-28 23:30:28 +0530

    Add 3x3 matrix initialization and printing

---
+++ 16matrix2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+    int i,j;
+
+    int arr[3][3];
+    arr[0][0]=1;
+    arr[0][1]=2;
+    arr[0][2]=3;
+
+    arr[1][0]=4;
+    arr[1][1]=5;
+    arr[1][2]=6;
+
+    arr[2][0]=7;
+    arr[2][1]=8;
+    arr[2][2]=9;
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+}
