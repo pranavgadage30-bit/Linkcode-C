ad3a4a89d120711f2336b92326f2e7dcec61173b
Pranav Gadage <pranavgadage30@gmail.com>
2026-03-31 19:38:11 +0530

    Add 3x3 matrix input and display functionality
    Implement a 3x3 matrix input and display program

---
+++ 17matrix3.c
@@ -0,0 +1,27 @@
+
+
+#include<stdio.h>
+
+int main(){
+
+    int i,j,arr[3][3];
+    
+    printf("Enter the elements in array:");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    
+    printf("\n--------Matrix-----------\n");
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",arr[i][j]);
+        
+        }
+        printf("\n");
+    }
+
+}17matrix3.c
+
