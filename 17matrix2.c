b7d1ff78c552157ff1dd5022a1eeb387af0de0d7
Pranav Gadage <pranavgadage30@gmail.com>
2026-03-31 19:37:18 +0530

    Create 17matrix2.c

---
+++ 17matrix2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+    int i,j;
+
+    int arr[3][3];
+    arr[0][0]=1;
+    arr[0][1]=2;
+    arr[0][2]=3;
+
+    arr[1][0]=4;
+    arr[1][1]=5;
+    arr[1][2]=6;
+
+    arr[2][0]=7;
+    arr[2][1]=8;
+    arr[2][2]=9;
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            if(arr[i][j]%2==0){
+            printf("%d ",arr[i][j]);
+            }
+        }
+        printf("\n");
+    }
+
+}
