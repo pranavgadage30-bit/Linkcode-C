ec813204d343646c014ede814b9d72a01b010ad2
Pranav Gadage <pranavgadage30@gmail.com>
2026-04-01 23:38:47 +0530

    Fix loop increments and output condition in 18matrix3.c
    Fix the nested loop increment syntax and correct the condition for printing elements.

---
+++ 18matrix3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    
+    int arr[3][3];
+    int i,j;
+    
+
+    printf("Enter elements in array:");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    
+    for(i=0;i<3;i++){
+        int sum=0;
+        for(j=0;j<3;j++){
+            if(i+j==2){
+                printf("%d",arr[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
