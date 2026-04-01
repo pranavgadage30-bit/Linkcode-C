9d4be39c00da51615a3c64e180195c3746b3d476
Pranav Gadage <pranavgadage30@gmail.com>
2026-04-01 23:38:21 +0530

    Implement 3x3 matrix diagonal printer
    This program takes a 3x3 matrix as input and prints its diagonal elements.

---
+++ 18matrix2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    
+    int arr[3][3];
+    int i,j;
+    
+
+    printf("Enter elements in array:");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    
+    for(i=0;i<3;i++){
+        int sum=0;
+        for(j=0;j<3;j++){
+            if(i==j){
+                printf("%d",arr[i][j]);
+            }
+        }
+        printf("\n");
+    
+    }
+}
