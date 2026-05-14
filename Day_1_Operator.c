c4e064ac2804d59f03ed049dda68fac04e9fff05
Pranav Gadage <pranavgadage30@gmail.com>
2026-05-14 23:08:30 +0530

    Add files via upload

---
+++ Day_1_Operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x=5,y=2;
+
+    printf("\n x+y = %d",x+y);
+    printf("\n x-y = %d",x-y);
+    printf("\n x*y = %d",x*y);
+    printf("\n x/y = %d",x/y);
+    printf("\n x%y = %d",x%y);
+
+
+    return 0;
+} 
